2720ddeefd405dcf9358ba121f4125608b8b424b
shawnmcrowley <scrowley@buffalo.edu>
2022-12-29 12:12:59 -0500

    Updated Index.html

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const mongoose = require('mongoose');
 const env = require('dotenv/config.js');
+const db = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}${process.env.MONGODB_URL}`;
+
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-const db = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}${process.env.MONGODB_URL}`;
+
 
 const Message = mongoose.model('Message', {
     name: String,
@@ -26,15 +28,13 @@ app.get('/messages', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
-    const message = new Message(req.body);
-    message.save((err) => {
-        if (err) {
-        res.sendStatus(500);
-        } else {
+    try {
+        const message = new Message(req.body);
+        const savedMessage = message.save();
         io.emit('message', req.body);
-        res.sendStatus(200);
-        }
-    })
+    } catch (error) {
+        res.sendStatus(500);
+    }
 })
 
 io.on('connection', (socket) => {
